a9f5a3bffb12a3f321c8df0ebb410d64a353bc29
kevbergstrom <kevbergstrom@gmail.com>
2021-09-27 17:09:02 -0700

    Update tests.js with binary search tests

--- javascript/tests.js
+++ javascript/tests.js
@@ -1,5 +1,3 @@
-const bubbleSort = require('./bubbleSort.js')
-
 function compareArrays(a, b){
     if(a.length != b.length) return false
     for(let i = 0;i < a.length; i++){
@@ -7,6 +5,8 @@ function compareArrays(a, b){
     }
     return true
 }
+
+const bubbleSort = require('./bubbleSort.js')
 
 function testBubbleSort(unsorted){
     let sorted = [...unsorted].sort()
@@ -18,4 +18,26 @@ function testBubbleSort(unsorted){
 }
 
 testBubbleSort([8,2,5,1,7,4,6,9,3])
+
+const binarySearch = require('./binarySearch.js')
+
+function testBinarySearch(sorted){
+    for(let i = 0; i < 100; i++){
+        let found = sorted.includes(i)
+        let foundIndex = binarySearch(sorted, i)
+        if(found){
+            console.assert(
+                sorted[foundIndex] == i, 
+                `[${arguments.callee.name}] Coudln't find ${i}`
+            )
+        }else{
+            console.assert(
+                foundIndex == -1, 
+                `[${arguments.callee.name}] Found index ${foundIndex} for nonexistent value ${i}`
+            )
+        }
+    }
+}
+
+testBinarySearch([1,1,5,5,7,8,9,100])
 
